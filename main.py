3f39195e4cabb697c53795757483cbcb890a88c2
Ciniaq\pmarc <pmarciniak000@gmail.com>
2025-02-24 21:36:27 +0100

    Added basic bounding boxes

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import cv2
 import numpy as np
 import matplotlib.pyplot as plt
 import sys
+from skimage.measure import label, regionprops
 
 csv_name = 'LegoParts.csv'
 colors_dict = {}
@@ -12,6 +13,10 @@ actual_colors_dict = {}
 
 images_path = r'D:\UELego\Lego\Saved\Screenshots\WindowsEditor'
 images = {}
+
+dataset_images_path = r'dataset/images'
+dataset_labels_path = r'dataset/labels'
+
 
 def fill_colors_dict():
     lego_parts_df = pd.read_csv(csv_name)
@@ -23,11 +28,22 @@ def fill_colors_dict():
         rgb_value = (color_dict['R'], color_dict['G'], color_dict['B'], 255)
         colors_dict[rgb_value] = row['---']
 
+
 def fill_images_dict():
     for file_name in os.listdir(images_path):
         name = file_name.split('_')[0]
         if '_masked' in file_name:
-            images[name] = [file_name.split('_')[0] + '_original.jpg', file_name]
+            images[name] = [file_name.split('_')[0] + '_original.png', file_name]
+
+def fill_actual_colors_dict(image):
+    actual_colors_dict.clear()
+    image_array = np.array(image)
+    pixels = image_array.reshape(-1, image_array.shape[-1])
+    unique_colors = np.unique(pixels, axis=0)
+
+    for color in unique_colors:
+        color_v = (int(color[0]), int(color[1]), int(color[2]), 255)
+        actual_colors_dict[color_v] = colors_dict[find_nearest_color(color_v)[0]]
 
 def find_nearest_color(input_color):
     nearest_value = 1000
@@ -39,24 +55,43 @@ def find_nearest_color(input_color):
             nearest_color = color
     return nearest_color, float(nearest_value)
 
+
 if __name__ == '__main__':
     fill_colors_dict()
     fill_images_dict()
 
     for key, (original, masked) in images.items():
-        image = Image.open(images_path+'\\'+masked)
-        print(masked)
-        width, height = image.size
-        bool_array = np.zeros((height, width), dtype=bool)
-
+        image = Image.open(images_path + '\\' + masked)
+        image2 = Image.open(images_path + '\\' + original)
         image_array = np.array(image)
-        pixels = image_array.reshape(-1, image_array.shape[-1])
-        unique_colors = np.unique(pixels, axis=0)
+        image_output = np.array(image2)
 
-        for color in unique_colors:
-            color_v = (int(color[0]), int(color[1]), int(color[2]), 255)
-            actual_colors_dict[color_v] = colors_dict[find_nearest_color(color_v)[0]]
-
+        fill_actual_colors_dict(image)
 
         for color in actual_colors_dict.keys():
-            print(color, actual_colors_dict[color])
+            if color == (0, 0, 0, 255):
+                continue
+            mask = cv2.inRange(image_array, color, color)
+            label_image = label(mask)
+            regions = regionprops(label_image)
+            for region in regions:
+                minr, minc, maxr, maxc = region.bbox
+                if minc == 0:
+                    continue
+                # cv2.polylines(image_output, [minr, minc, maxr, maxc], isClosed=False, color=color, thickness=2)
+                # image_output[minr:maxr, minc:maxc] = color
+                cv2.rectangle(image_output, (minc, minr), (maxc, maxr), color, 2)
+                # break
+
+        image1 = Image.fromarray(image_output)
+        image1.show()
+        image1.save(original.split('.')[0] + '_boxes.png')
+
+            # break
+        break
+
+
+
+
+
+
