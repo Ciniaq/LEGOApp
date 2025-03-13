326a9480a202c7bf149b89297c4b4a8cc78f9e25
Ciniaq\pmarc <pmarciniak000@gmail.com>
2025-03-13 18:41:53 +0100

    Added script to change all classes in labels to zero class

--- test.py
+++ test.py
@@ -3,20 +3,21 @@ import matplotlib.pyplot as plt
 from ultralytics import YOLO
 
 # Load the trained model
-model_path = "D:\Pycharm\\UnlitToBounds\\trained_model1.pt"
+model_path = "D:\Pycharm\\UnlitToBounds\\runs2\\train\\exp\\weights\\last.pt"
 model = YOLO(model_path, task="detect")
 
 # Load the image
-# image_path = "D:\Pycharm\\UnlitToBounds\\real_images\\004.jpg"
-image_path = "D:\Pycharm\\UnlitToBounds\\dataset\\images\\train\\9-1503_original.png"
+image_path = "D:\Pycharm\\UnlitToBounds\\real_images\\004.jpg"
+# image_path = "D:\Pycharm\\UnlitToBounds\\dataset\\images\\val\\9-1503_original.png"
 image = cv2.imread(image_path)
 image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
 
 # Perform object detection
-results = model.predict(image, conf=0.4)
-
+results = model.predict(image, conf=0.5)
+# 416
 annotated_image = results[0].plot(font_size=10, line_width=5)
 
 plt.imshow(annotated_image)
 plt.axis('off')
 plt.show()
+plt.imsave("output.png", annotated_image)
--- training.py
+++ training.py
@@ -3,20 +3,20 @@ from ultralytics import YOLO
 yaml_path = "D:\Pycharm\\UnlitToBounds\dataset\yolo11.yaml"
 model_path = "D:\Pycharm\\UnlitToBounds\\trained_model3.pt"
 
-model = YOLO("yolo11n.pt", task="detect")
+model = YOLO(model_path, task="detect")
 
 model.train(
     data=yaml_path,
-    epochs=75,
+    epochs=60,
     imgsz=(480, 270),
-    batch=4,
+    batch=1,
     device="cpu",
     patience=10,
     augment=True,
     mosaic=0,
     save_period=1,
     project="runs2/train",
-    name="exp",
+    name="exp2",
 )
 
 model.save(model_path)
