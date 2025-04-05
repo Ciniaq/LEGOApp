866363affd194642d16c5d4a119c20c4175641c5
Ciniaq\pmarc <pmarciniak000@gmail.com>
2025-04-05 13:26:39 +0200

    Add predicted procent

--- classifier_files/app/camera_app.py
+++ classifier_files/app/camera_app.py
@@ -1,14 +1,13 @@
-import pickle
-import random
+import json
 import threading
 
 import cv2
 import numpy as np
-import timm
 import torch
+import torchvision.transforms as transforms
 from sahi import AutoDetectionModel
 from sahi.predict import get_sliced_prediction
-from torchvision import transforms
+from torchvision import models
 from ultralytics import YOLO
 
 lock = threading.Lock()
@@ -25,19 +24,36 @@ detection_model = AutoDetectionModel.from_pretrained(
     confidence_threshold=0.8,
     device='cuda:0',
 )
-model_path = "../vit_lego_2_1_1.pth"
-model = timm.create_model("vit_base_patch16_384", pretrained=False, num_classes=46)
-model.load_state_dict(torch.load(model_path, map_location=torch.device('cpu')))
+
+# model_path = "../vit_lego_2_1_1.pth"
+# model = timm.create_model("vit_base_patch16_384", pretrained=False, num_classes=46)
+# model.load_state_dict(torch.load(model_path, map_location=torch.device('cpu')))
+# transform = transforms.Compose([
+#     transforms.Resize((384, 384)),
+#     transforms.ToTensor(),
+#     transforms.Normalize(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5])
+# ])
+# with open('../idx_to_class.pkl', 'rb') as f:
+#     idx_to_class = pickle.load(f)
+with open("../class_to_idx.json", "r") as f:
+    class_to_idx = json.load(f)
+idx_to_class = {v: k for k, v in class_to_idx.items()}
 transform = transforms.Compose([
-    transforms.Resize((384, 384)),
+    transforms.Resize((224, 224)),
     transforms.ToTensor(),
-    transforms.Normalize(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5])
+    transforms.Normalize(mean=[0.485, 0.456, 0.406],
+                         std=[0.229, 0.224, 0.225]),
 ])
-with open('../idx_to_class.pkl', 'rb') as f:
-    idx_to_class = pickle.load(f)
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+model = models.resnet18(pretrained=False)
+model.fc = torch.nn.Linear(model.fc.in_features, len(idx_to_class))
+model.load_state_dict(torch.load("../resnet_epoch_5.pth", map_location=device))
+model = model.to(device)
+model.eval()
 
 
-def resize_and_pad_image(cropped_img, target_size=384):
+def resize_and_pad_image(cropped_img, target_size=224):
     h, w, _ = cropped_img.shape
 
     if h > w:
@@ -84,14 +100,21 @@ def process_frame():
                 ready_image = resize_and_pad_image(cropped)
                 pil_image = transforms.ToPILImage()(ready_image)
                 image_tensor = transform(pil_image).unsqueeze(0)
-                outputs = model(image_tensor)
-                logits = outputs
-                predicted_class = logits.argmax(-1).item()
-                cv2.putText(overlay, str(idx_to_class[predicted_class]), (int(bbox.minx), int(bbox.miny),),
-                            cv2.FONT_HERSHEY_SIMPLEX,
-                            1, (random.randint(50, 300), random.randint(50, 300), random.randint(50, 300)), 3)
-                cv2.rectangle(overlay, (int(bbox.minx), int(bbox.miny)), (int(bbox.maxx), int(bbox.maxy)), (0, 255, 0),
-                              2)
+                input_tensor = image_tensor.to(device)
+                with torch.no_grad():
+                    outputs = model(input_tensor)
+                    probabilities = torch.nn.functional.softmax(outputs, dim=1)
+                    confidence, predicted = torch.max(probabilities, 1)
+                    confidence_predicted = confidence.item() * 100
+                    if confidence_predicted > 60:
+                        predicted_class = idx_to_class[predicted.item()]
+                        cv2.putText(overlay, f"{predicted_class} {confidence_predicted:.2f}%",
+                                    (int(bbox.minx), int(bbox.miny),),
+                                    cv2.FONT_HERSHEY_SIMPLEX,
+                                    1, (255, 0, 255), 1)
+                        cv2.rectangle(overlay, (int(bbox.minx), int(bbox.miny)), (int(bbox.maxx), int(bbox.maxy)),
+                                      (0, 255, 0),
+                                      2)
             processed = True
 
 
@@ -132,7 +155,7 @@ if __name__ == '__main__':
                     processed = False
             lock.release()
 
-        blended = cv2.addWeighted(new_frame, 1, overlay, 0.9, 0)
+        blended = cv2.addWeighted(new_frame, 1, overlay, 1, 1)
 
         cv2.imshow("LegoDetection", blended)
 
--- classifier_files/train_rn.py
+++ classifier_files/train_rn.py
@@ -8,17 +8,15 @@ import torchvision.transforms as transforms
 from torch.utils.data import DataLoader
 from torchvision import models, datasets
 
-# Sprawdzenie dostępności GPU
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-# Transformacje danych
 transform = transforms.Compose([
+    transforms.Grayscale(num_output_channels=1),
     transforms.Resize((224, 224)),
     transforms.ToTensor(),
-    transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+    transforms.Normalize(mean=[0.5], std=[0.5]),
 ])
 
-# Załadowanie własnego zbioru danych
 data_dir = "./dataset224"
 trainset = datasets.ImageFolder(root=f"{data_dir}/train", transform=transform)
 trainloader = DataLoader(trainset, batch_size=32, shuffle=True)
@@ -26,23 +24,22 @@ trainloader = DataLoader(trainset, batch_size=32, shuffle=True)
 valset = datasets.ImageFolder(root=f"{data_dir}/val", transform=transform)
 valloader = DataLoader(valset, batch_size=32, shuffle=False)
 
-# Zapisanie mapowania indeksów do nazw klas
 class_to_idx = trainset.class_to_idx
 with open("class_to_idx.json", "w") as f:
     json.dump(class_to_idx, f)
 
-# Załadowanie modelu ResNet
 model = models.resnet18(pretrained=True)
 num_ftrs = model.fc.in_features
-model.fc = nn.Linear(num_ftrs, len(trainset.classes))  # Dostosowanie do liczby klas
+model.fc = nn.Linear(num_ftrs, len(trainset.classes))
+model.conv1 = nn.Conv2d(1, model.conv1.out_channels, kernel_size=model.conv1.kernel_size, stride=model.conv1.stride,
+                        padding=model.conv1.padding, bias=False)
+
 model = model.to(device)
 
-# Definicja funkcji straty i optymalizatora
 criterion = nn.CrossEntropyLoss()
 optimizer = optim.Adam(model.parameters(), lr=0.001)
 
-# Trenowanie modelu
-num_epochs = 5
+num_epochs = 15
 for epoch in range(num_epochs):
     model.train()
     running_loss = 0.0
@@ -62,9 +59,8 @@ for epoch in range(num_epochs):
         sys.stdout.flush()
 
     print(f"\nEpoch {epoch + 1}, Loss: {running_loss / total_batches:.4f}")
-    torch.save(model.state_dict(), f"resnet_epoch_{epoch + 1}.pth")  # Zapis modelu po każdej epoce
+    torch.save(model.state_dict(), f"resnet_grayscale_{epoch + 1}.pth")  # Zapis modelu po kaĹĽdej epoce
 
-# Testowanie modelu
 model.eval()
 correct = 0
 total = 0
@@ -78,5 +74,4 @@ with torch.no_grad():
 
 print(f'Accuracy: {100 * correct / total:.2f}%')
 
-# Zapis końcowego modelu
-torch.save(model.state_dict(), "resnet_final.pth")
+torch.save(model.state_dict(), "resnet_grayscale.pth")
